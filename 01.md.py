4f5baf34b87d7dfec08ead4f38e3b45799c9c74a
yhd0210 <yhd060210@gmail.com>
2022-09-19 21:51:35 +0900

    Study.D1.Python

---
+++ 01.md.py
@@ -0,0 +1,126 @@
+# 기초 
+
+# print("Hello World")
+
+#print(100+200)
+
+#print("난생처음 파이썬을")
+#print("열심히 공부 중입니다~")
+#print("이젠 저도 파이썬 프로그래머입니다. ^^")
+
+# 변수
+
+#n = 100+200
+
+#a = 100
+#b = 200
+#c = a + b
+
+#print(n)
+#print(c)
+
+#str1 = "난생처음"
+#str2 = '파이썬'
+
+#print(str1,str2)
+
+# 계산기
+
+# n1 = 100 + 200
+# n2 = 100 - 200
+# n3 = 100 * 200
+# n4 = 100 / 200
+
+# print("100 + 200 = ", n1)
+# print("100 - 200 = ", n2)
+# print("100 * 200 = ", n3)
+# print("100 / 200 = ", n4)
+
+# input() 함수
+
+#num2 = input()
+
+#print(num2)
+
+#num1 = int(input())
+#num2 = int(input())
+
+#result = num1 + num2
+
+#print(result)
+
+# 택배 배송 정보 입력
+
+# name = input("받는 사람 : ")
+# address__ = input("주소 : ")
+# weigth = int(input("무게(g) : "))
+
+# print("받는 사람 ==> ",name)
+# print("주소 ==> ",address__)
+# print("배송비 ==> ", weigth*5)
+
+# 연산자
+
+# num1 = int(input("나눠지는 수 ==> "))
+# num2 = int(input("나누는 수 ==> "))
+
+# print(num1,"을(를)",num2,"(으)로 나눈 몫은 ",num1//num2," 입니다.")
+# print(num1,"을(를)",num2,"(으)로 나눈 나머지는 ",num1%num2," 입니다.")
+
+# num1 = 100, 200
+# print(num1) #tuple (100, 200)
+
+# 기말 평균 학점
+
+# Python = 3
+# Mobile = 2
+# Excel_ = 1
+
+# A = 4.5
+# A0 = 4.0
+# B = 3.5
+
+# avg = ((Python * B)+(Mobile * A0)+(Excel_ * A))/(Python+Mobile+Excel_)
+
+# print(avg)
+
+# 데이터형, 문자열
+
+#var1 = "난생"*3
+#print(var1)
+
+# 문자열 비교
+
+# var1 = input("첫 번째 문자열 ==> ")
+# var2 = input("두 번째 문자열 ==> ")
+
+# n = len(var1) - len(var2)
+
+# print("두 문자열의 길이 차이는 ", n, "입니다")
+
+# 문자열 거꾸로 출력
+
+# ss = '트와이스'
+
+# print("원방향 : " , ss)
+# print("반대방향 : ", ss[3], end='')
+# print(ss[2],end='')
+# print(ss[1],end='')
+# print(ss[0],end='')
+
+# 조건문 if
+
+# num = 99
+# if num < 100:
+#     print("100보다 작습니다.")
+
+# 숫자 크기 분류
+
+# num = int(input())
+
+# if num < 100:
+#     print("100보다 작군요.")
+#     print("여기는 참입니다.")
+# else:
+#     print("100보다 크군요.")
+#     print("여기는 거짓입니다.")
