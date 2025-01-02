bc5e478ca86264b4797ba837b0438e4b521f368c
ryu <yhd060210@gmail.com>
2025-01-02 13:46:15 +0900

    Changes

---
+++ Java/Hello_Java/src/chapter02/Scaninteger.java
@@ -0,0 +1,10 @@
+package chapter02;
+
+import java.util.Scanner;
+
+public class Scaninteger {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
