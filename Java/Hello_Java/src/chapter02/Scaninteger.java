5d54952163182e03b02cf533a6246c437e79e86d
ryu <yhd060210@gmail.com>
2025-01-02 14:24:31 +0900

    ..

--- Java/Hello_Java/src/chapter02/Scaninteger.java
+++ Java/Hello_Java/src/chapter02/Scaninteger.java
@@ -11,11 +11,25 @@ public class Scaninteger {
         // primitive type (기본, 원시 자료형)
         int value; // 정수 값을 담을 변수
 
-        System.out.println("정수값 : ");
+        System.out.print("정수값 : ");
 
         // keyboard 를 통해 입력한 변수를 받는다
         value = sc.nextInt();
 
-        System.out.println(value + "를 입력했습니다.");
+        System.out.printf("%d를 입력했습니다\n", value);
+
+        // keyboard 로 입력한 정수 값에 10을 더하거나 뺀 값을 표시
+        System.out.printf("10을 더한 값은 %d 입니다\n",(value+10));
+        System.out.printf("10을 뺀 값은 %d 입니다\n", (value-10));
+
+        // keyboard 로 엽력한 정수 값에 10을 곱하거나 나눈 값을 표시
+        System.out.printf("10을 곱한 값은 %d 입니다\n", value*10);
+        System.out.printf("10을 나눈 값은 %d 입니다\n", (value/10));
+
+        // keyboard 로 입력한 정수 값에 10으로 나눈 나머지 표시
+        System.out.printf("10으로 나머지 연산한 값은 %d 입니다\n",value%10);
+
+        // keyboard 로 입력한 정수 값을 음수로 변환
+        System.out.printf("음수로 변환한 값은 %d입니다\n", -value);
     }
 }
